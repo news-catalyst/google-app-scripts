dc7c50000cdb91b5919a33e2b4efb3c5f4271380
Jacqui Lough <lough.jacqui@gmail.com>
2020-08-19 14:12:22 +1000

    Store preview variables in script config instead of hardcoding

--- Code.js
+++ Code.js
@@ -184,6 +184,7 @@ function setScriptConfig(data) {
   for (var key in data) {
     scriptProperties.setProperty(key, data[key]);
   }
+  return "Saved configuration.";
 }
 
 /*
@@ -278,7 +279,7 @@ function storeLocaleID(localeID) {
 
 function getArticleSlug() {
   return getValue('ARTICLE_SLUG');
-}
+}  
 
 function storeArticleSlug(slug) {
   storeValue("ARTICLE_SLUG", slug);
@@ -434,8 +435,6 @@ function getArticleMeta() {
   Logger.log("getArticleMeta START");
   var articleID = getArticleID();
 
-  var isLatestVersionPublished = getLatestVersionPublished();
-
   var publishingInfo = getPublishingInfo();
   Logger.log("publishingInfo: ", publishingInfo);
 
@@ -470,10 +469,30 @@ function getArticleMeta() {
 
   var seoData = getSEO();
 
+  var scriptConfig = getScriptConfig();
+  var previewUrl = scriptConfig['PREVIEW_URL']
+  var previewSecret = scriptConfig['PREVIEW_SECRET'];
+  var accessToken = scriptConfig['ACCESS_TOKEN'];
+  var contentApi = scriptConfig['CONTENT_API'];
+  var personalAccessToken = scriptConfig['PERSONAL_ACCESS_TOKEN'];
+  var graphqlApi = scriptConfig['GRAPHQL_API'];
+  var awsAccessKey = scriptConfig['AWS_ACCESS_KEY_ID'];
+  var awsSecretKey = scriptConfig['AWS_SECRET_KEY'];
+  var awsBucket = scriptConfig['AWS_BUCKET'];
+
   if (typeof(articleID) === "undefined" || articleID === null) {
     Logger.log("articleID is undefined, returning new doc state");
 
     return {
+      awsAccessKey: awsAccessKey,
+      awsSecretKey: awsSecretKey,
+      awsBucket: awsBucket,
+      graphqlApi: graphqlApi,
+      personalAccessToken: personalAccessToken,
+      accessToken: accessToken,
+      contentApi: contentApi,
+      previewUrl: previewUrl,
+      previewSecret: previewSecret,
       articleID: null,
       headline: headline,
       byline: byline,
@@ -487,7 +506,17 @@ function getArticleMeta() {
       seo: seoData
     }
   }
+
   var articleMetadata = {
+    awsAccessKey: awsAccessKey,
+    awsSecretKey: awsSecretKey,
+    awsBucket: awsBucket,
+    graphqlApi: graphqlApi,
+    personalAccessToken: personalAccessToken,
+    accessToken: accessToken,
+    contentApi: contentApi,
+    previewUrl: previewUrl,
+    previewSecret: previewSecret,
     articleID: articleID,
     headline: headline,
     byline: byline,
@@ -518,10 +547,14 @@ function handlePreview(formObject) {
 
   // construct preview url
   var slug = getArticleSlug();
-  var previewUrl = "http://localhost:3000/api/preview?secret=DUNGEONSDRAGONS&slug=" + slug;
+
+  var scriptConfig = getScriptConfig();
+  var previewHost = scriptConfig['PREVIEW_URL'];
+  var previewSecret = scriptConfig['PREVIEW_SECRET'];
+  var fullPreviewUrl = previewHost + "?secret=" + previewSecret + "&slug=" + slug;
 
   // open preview url in new window
-  message += "<br><a href='" + previewUrl + "' target='_blank'>Preview article in new window</a>"
+  message += "<br><a href='" + fullPreviewUrl + "' target='_blank'>Preview article in new window</a>"
   return message;
 }
 
