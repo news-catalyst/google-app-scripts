e5809a89b072cdd96c96962d25aeaae090e5155b
Jacqui Lough <lough.jacqui@gmail.com>
2021-10-05 09:15:55 +1100

    bugfix: publish date shouldn't apply to pages

--- GraphQL.js
+++ GraphQL.js
@@ -338,6 +338,21 @@ const insertPageGoogleDocsMutationWithoutId = `mutation AddonInsertPageGoogleDoc
     returning {
       id
       slug
+      page_translations {
+        content
+        facebook_description
+        facebook_title
+        first_published_at
+        headline
+        id
+        last_published_at
+        locale_code
+        published
+        search_description
+        search_title
+        twitter_description
+        twitter_title
+      }
       page_google_documents {
         id
         google_document {
@@ -358,6 +373,21 @@ const insertPageGoogleDocsMutation = `mutation AddonInsertPageGoogleDocWithID($i
     returning {
       id
       slug
+      page_translations {
+        content
+        facebook_description
+        facebook_title
+        first_published_at
+        headline
+        id
+        last_published_at
+        locale_code
+        published
+        search_description
+        search_title
+        twitter_description
+        twitter_title
+      }
       page_google_documents {
         id
         google_document {
