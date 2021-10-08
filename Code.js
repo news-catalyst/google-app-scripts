9e60836883fa193ff3726865ebaddcc184d36a4a
Jacqui Lough <lough.jacqui@gmail.com>
2021-10-08 15:17:45 +1100

    only trim the end of the line, use trimEnd() not trim()

--- Code.js
+++ Code.js
@@ -1727,7 +1727,7 @@ async function processDocumentContents(activeDoc, document, slug) {
               childElement.link = subElement.textRun.textStyle.link.url;
             }
             if (inSpecialFormatBlock) {
-              childElement.content = subElement.textRun.content.trim();
+              childElement.content = subElement.textRun.content.trimEnd();
             } else {
               childElement.content = cleanContent(subElement.textRun.content); 
             }
